96cbc50f7ea191e2ed3ca747e8e3b19a5273c5c2
AH RIAD <ahriad231@gmail.com>
2021-02-19 00:27:46 +0600

    added some code

---
+++ letconst.js
@@ -0,0 +1,13 @@
+const hubby = "Eliash Kanchon";
+
+console.log(hubby);
+const numbers = [12,45];
+numbers[1] = 88;
+numbers.push(34);
+console.log(numbers);
+const nayok ={
+    name:"shakib khan", phone :45
+};
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khal";
+ 
---
+++ templete.js
@@ -0,0 +1,12 @@
+const firstName = "Justin";
+const lastName = "Biber";
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName} is a good boy`
+console.log(fullName2);
+
+
+
+const multiLine = `I Love you
+tor maire bap
+halar put`;
+console.log(multiLine);
